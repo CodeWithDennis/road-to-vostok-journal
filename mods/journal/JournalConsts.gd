cadd0be070025b349645f697f848cc6ba4e413a4
CodeWithDennis <personal.elsinga@gmail.com>
2026-05-03 13:46:55 +0200

    chore(journal): add shared constants module

---
+++ mods/journal/JournalConsts.gd
@@ -0,0 +1,14 @@
+extends Object
+
+const INPUT_ACTION_TOGGLE := "journal_toggle"
+const KEY_JSON_VERSION := "version"
+const KEY_JSON_TEXT := "text"
+const KEY_JSON_TABS := "tabs"
+const KEY_LEGACY_TAB_TITLE := "title"
+const KEY_LEGACY_TAB_TEXT := "text"
+const SAVE_FORMAT_VERSION := 2
+const SAVE_FORMAT_VERSION_TABS := 1
+const GAME_THEME_PATH := "res://UI/Themes/Theme.tres"
+const MCM_BOOL_PAUSE_GAME := "pause_game_while_open"
+const RUNTIME_META_PAUSE_GAME := "JournalMod/pause_game_while_open"
+const GAME_DATA_PATH := "res://Resources/GameData.tres"
